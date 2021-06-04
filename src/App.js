5d6baa40cb0311346f5eded8bd46e8b05568f93d
sir-udenna <udennaboy@gmail.com>
2021-06-04 00:28:49 -0500

    new

--- src/App.js
+++ src/App.js
@@ -5,32 +5,19 @@ import Home from './Home/Home'
 import { Switch, Route } from 'react-router-dom';
 import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import { withRouter } from "react-router-dom";
-
-
-// Redirect, Link, Router,
-// let userLocation
-
-// const success = (data) =>{
-// 	console.log(data, 'success message')
-// 	userLocation = [data.coords.latitude,data.coords.longitude]
-// }
-
-// const fail = (data) =>{
-// 	console.log(data, 'fail message')
-// }
-
-// window.navigator.geolocation.getCurrentPosition(success,fail)
+import Info from './Info/Info'
 
 class App extends React.Component {
-
-constructor(props){
-  super(props)
+  constructor(props) {
+    super(props)
     this.state = {
       loggedIn: false,
       resturaunts: [],
-      location: {longitude: -95, latitude: 30}
+      location: { longitude: 0, latitude: 0 },
+      term: '',
+      info: {}
+    }
   }
-}
 
   handleLogin = (e) => {
 
@@ -58,42 +45,38 @@ constructor(props){
         this.props.history.push("/home")
       })
   }
+
   handleLogout = () => {
     console.log('clear localstorage')
     localStorage.clear()
+    window.location.href = '/login';
   }
 
   getResturaunts = () => {
-    let userLocation
 
-      const success = (data) => {
-      console.log(data, 'success message')
-      this.userLocation = [data.coords.latitude, data.coords.longitude]
+    const success = (data) => {
+      this.setState({ location: { longitude: data.coords.longitude, latitude: data.coords.latitude } })
+      console.log(data, 'success')
+
+      fetch(`http://localhost:3000/api/v1/yelp_restaurants/?lat=${data.coords.latitude}&long=${data.coords.longitude}&term=${this.state.term} food`, {
+
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer TfAfZ9CEBvQhMOvO5CcgbnmVNh4raReuVs6pQ5T9feqpz8_QmXw-R1kWfxJTvMoE5d7mGrencCzHmRCR2alxB2whr8GMm0byjI22x0kia_u2TgHwPLdrwO31RBSwYHYx'
+        }
+      })
+        .then(res => res.json())
+        .then(data => this.setState({ resturaunts: data.result.businesses }))
+
     }
 
     const fail = (data) => {
-      console.log(data, 'fail message')
+      console.log(data, 'fail')
     }
 
     window.navigator.geolocation.getCurrentPosition(success, fail)
-
-    if(userLocation){
-      console.log('found location')
-      console.log(userLocation)
-    }
-
-    fetch(`http://localhost:3000/api/v1/yelp_restaurants/?lat=${this.state.location.latitude}&long=${this.state.location.longitude}`, {
-
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': 'Bearer TfAfZ9CEBvQhMOvO5CcgbnmVNh4raReuVs6pQ5T9feqpz8_QmXw-R1kWfxJTvMoE5d7mGrencCzHmRCR2alxB2whr8GMm0byjI22x0kia_u2TgHwPLdrwO31RBSwYHYx'
-      }
-    })
-      .then(res => res.json())
-      .then(data => this.setState({ resturaunts: data.result.businesses }))
-    console.log(this.state.resturaunts, 'rest')
 
     if (this.state.loggedIn === false) {
       this.setState({ loggedIn: true })
@@ -102,27 +85,43 @@ constructor(props){
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getResturaunts()
   }
 
+  handleSearch = (e) => {
+    this.setState({ term: e.target.value })
+    this.getResturaunts()
+  }
+
+  handleMoreinfo = (card) => {
+    console.log(card.id)
+      localStorage.setItem("infoData", card.id);
+      window.location.href = '/info';
+  }
+
   render() {
-    console.log(this.state.location.longitude,this.state.location.latitude, "state")
     return (
       <div className="App">
         {localStorage.token ? (
           <Switch>
             <Route exact path="/home">
-              <Home allResturaunts={this.state.resturaunts} handleLogout={this.handleLogout} />
+              <Home handleMoreinfo={this.handleMoreinfo} allResturaunts={this.state.resturaunts} handleLogout={this.handleLogout} term={this.state.term} handleSearch={this.handleSearch} />
+            </Route>
+            <Route exact path="/info">
+              <Info info={this.state.info} />
             </Route>
           </Switch>
         ) : (
           <Switch>
+            <Route exact path="/home">
+              <LogIn handleLogin={this.handleLogin} />
+            </Route>
             <Route exact path="/login">
               <LogIn handleLogin={this.handleLogin} />
-              <Redirect to="/login" handleLogin={this.handleLogin} />
             </Route>
           </Switch>
+
         )}
       </div>
     )
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,20 +1,16 @@
-
-import React
-  // , {useState}  
-  from 'react';
-import AppBar from '@material-ui/core/AppBar';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import InputBase from '@material-ui/core/InputBase';
+import NavBar from '../AppBar/PrimarySearchAppBar'
 
 function Copyright() {
   return (
@@ -48,9 +44,25 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'column',
   },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
   cardMedia: {
     paddingTop: '56.25%', // 16:9
   },
+
   cardContent: {
     flexGrow: 1,
   },
@@ -58,21 +70,30 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(6),
   },
+
+
+  inputRoot: {
+    color: 'inherit',
+  },
+
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+
 }));
 
 export default function Home(props) {
   const classes = useStyles();
   return (
     <React.Fragment>
-      <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          <Button variant="contained" color="primary" onClick={() => props.handleLogout()}>
-            Log Out
-          </Button>
-        </Toolbar>
-      </AppBar>
-
+      <NavBar classes={classes} logout={props.handleLogout} />
       <main>
         {/* Hero unit */}
         <div className={classes.heroContent}>
@@ -81,19 +102,22 @@ export default function Home(props) {
               Welcome to EATS.
             </Typography>
             <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              Here are some food near you, click one of the buttons below to get started
+              Here are some food near you, search below to personalize your results
             </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">
                 <Grid item>
-                  <Button variant="contained" color="primary" >
-                    Personalize food near me
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button variant="contained" color="primary">
-                    Suprise me
-                  </Button>
+                  <div className={classes.search}>
+                    <InputBase
+                      placeholder="Search…"
+                      classes={{
+                        root: classes.inputRoot,
+                        input: classes.inputInput,
+                      }}
+                      inputProps={{ 'aria-label': 'search' }}
+                      onChange={props.handleSearch}
+                    />
+                  </div>
                 </Grid>
               </Grid>
             </div>
@@ -101,16 +125,14 @@ export default function Home(props) {
         </div>
         <Container className={classes.cardGrid} maxWidth="md">
           {/* End hero unit */}
-          <Grid container spacing={5}>
-            {props.allResturaunts.map((card) => (
 
+          <Grid container spacing={5}>
+            {props.allResturaunts.reverse().map((card) => (
               <Grid item key={card.id} xs={12} sm={6} md={4} >
-                {/*----------------------------------------card----------------------------------------*/}
                 <Card className={classes.card}>
-                  {/* {console.log(props.allResturaunts.length, "Count")} */}
                   <CardMedia
                     className={classes.cardMedia}
-                    image={card.image_url}
+                    image={card.image_url === '' ? 'https://www.officespacesny.com/wp-content/themes/realestate-7/images/no-image.png' : card.image_url}
                   />
                   <CardContent className={classes.cardContent}>
                     <Typography gutterBottom variant="h5" component="h2">
@@ -119,18 +141,16 @@ export default function Home(props) {
                     <Typography>
                       {card.name}
                     </Typography>
-
                   </CardContent>
                   <CardActions>
-                    <Button size="small" color="primary">
+                    <Button size="small" color="primary" onClick={() => props.handleMoreinfo(card)}>
                       More Info
                     </Button>
-                    <Button size="small" color="primary">
+                    <Button size="small" color="primary" >
                       Favorite
                     </Button>
                   </CardActions>
                 </Card>
-                {/*---------------------------------------card end---------------------------------------*/}
               </Grid>
             ))}
           </Grid>
